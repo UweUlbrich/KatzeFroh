0b2e3586434d53cf3748fa6e3e3f06e7dcc63b95
Uwe Ulbrich <uwe.ulbrich@outlook.de>
2025-10-23 21:40:12 +0200

    Update relay pin to GPIO22 and blink example

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 // On many ESP32 D1 mini boards the onboard LED is connected to GPIO 2.
 
 const uint8_t LED_PIN = 2;      // change if your board uses a different pin
-const unsigned long INTERVAL = 500; // milliseconds
+const uint8_t RELAY_PIN = 22;   // relay pin set to GPIO22
+const unsigned long INTERVAL = 1000; // milliseconds
 
 unsigned long previousMillis = 0;
 bool ledState = LOW;
@@ -14,6 +15,7 @@ void setup() {
   Serial.begin(115200);
   delay(10);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(LED_PIN, ledState);
   Serial.println("Blink example started");
 }
@@ -24,6 +26,7 @@ void loop() {
     previousMillis = currentMillis;
     ledState = !ledState;
     digitalWrite(LED_PIN, ledState);
+    digitalWrite(RELAY_PIN, ledState); // also toggle the relay pin
     Serial.printf("LED is now %s\n", ledState ? "ON" : "OFF");
   }
 }
