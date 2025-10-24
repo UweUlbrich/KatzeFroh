254076d4b20252768c26b99477c61e5132d16db5
Uwe Ulbrich <uwe.ulbrich@outlook.de>
2025-10-24 19:36:15 +0200

    Switch follows GPIO32; remove blink; enable INPUT_PULLDOWN

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,26 @@
 #include <Arduino.h>
-#include <Arduino.h>
 
 // Blink the onboard LED of the AZ-Delivery / Wemos D1 Mini ESP32
 // On many ESP32 D1 mini boards the onboard LED is connected to GPIO 2.
 
 const uint8_t LED_PIN = 2;      // change if your board uses a different pin
-const uint8_t RELAY_PIN = 22;   // relay pin set to GPIO22
-const unsigned long INTERVAL = 1000; // milliseconds
-
-unsigned long previousMillis = 0;
-bool ledState = LOW;
+const uint8_t SWITCH_PIN = 32;   // change to the GPIO where your switch is connected
 
 void setup() {
   Serial.begin(115200);
   delay(10);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(LED_PIN, ledState);
-  Serial.println("Blink example started");
+  // Switch is wired to 3.3V when closed, use internal pull-down so the pin reads LOW when open
+  pinMode(SWITCH_PIN, INPUT_PULLDOWN);
+  Serial.println("Example started");
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= INTERVAL) {
-    previousMillis = currentMillis;
-    ledState = !ledState;
-    digitalWrite(LED_PIN, ledState);
-    digitalWrite(RELAY_PIN, ledState); // also toggle the relay pin
-    Serial.printf("LED is now %s\n", ledState ? "ON" : "OFF");
+  // Read the switch state and mirror it to the LED
+  int switchState = digitalRead(SWITCH_PIN);
+  if (switchState == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
   }
 }
